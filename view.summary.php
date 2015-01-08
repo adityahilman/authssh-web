e96a9a5cd96c989f8bec3c1207f9f2e58e344b96
adit <adit@addotech>
2015-01-08 20:48:30 +0700

    disable ajax.googleapis for css

--- view.summary.php
+++ view.summary.php
@@ -6,14 +6,14 @@
 	<title>SSH Two Factor Authentication - Admin Login</title>
 	
 	<!-- Stylesheets -->
-	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet'>
+	<!-- <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet'> -->
 	<link rel="stylesheet" href="css/style.css">
 	
 	<!-- Optimize for mobile devices -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	
 	<!-- jQuery & JS files -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> -->
 	<script src="js/script.js"></script>  
 </head>
 <body>
