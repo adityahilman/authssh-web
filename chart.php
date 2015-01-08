f3c1d01e98babd8c0814988eeb146276c390c3bf
adit <adit@addotech>
2015-01-08 18:57:40 +0700

    update for bar chart.php

--- chart.php
+++ chart.php
@@ -16,10 +16,18 @@ $data = new ViewLog();
 $list = $data->getUserChartSummaryLog();
 
 $listSuccess = $data->getUserSuccessChartSummaryLog();
+$listFailed = $data->getUserFailedChartSummaryLog();
 
 $dataSuccess = array();
+$dataSuccessTotal = array();
+
+$dataFailed = array();
+$dataFailedTotal = array();
+
 $dataStatus = array();
 $dataTotal = array();
+
+$xLabel = array("Login Summary");
 
 while ($data = mysql_fetch_array($list))
 {
@@ -30,7 +38,14 @@ while ($data = mysql_fetch_array($list))
 
 while ($data = mysql_fetch_array($listSuccess))
 {
-    
+    array_unshift($dataSuccess, $data['USER_LOGIN_STATUS']);
+    array_unshift($dataSuccessTotal, $data['COUNT(*)']);
+}
+
+while ($data = mysql_fetch_array($listFailed))
+{
+    array_unshift($dataFailed, $data['USER_LOGIN_STATUS']);
+    array_unshift($dataFailedTotal, $data['COUNT(*)']);
 }
 
 $graph = new Graph(600,400,"auto");
@@ -41,26 +56,26 @@ $graph->img->SetMargin(50,100,20,100);
 $graph->title->Set("Linux Login");
 $graph->SetShadow();
 
-$graph->xaxis->title->Set("Login Status");
+//$graph->xaxis->title->Set("Login Status");
 $graph->xaxis->SetLabelAlign("center");
-$graph->xaxis->SetTickLabels($dataStatus);
+$graph->xaxis->SetTickLabels($xLabel);
 
 $graph->yaxis->title->Set("Login Count");
 $graph->title->SetFont(FF_FONT1, FS_BOLD);
 
-$bplot1 = new BarPlot($dataTotal);
+$bplot1 = new BarPlot($dataSuccessTotal);
 $bplot1->SetFillColor("blue");
 $bplot1->value->show();
-//$bplot1->SetLegend("Success");
+$bplot1->SetLegend("Success");
 
-/*
-$bplot2 = new BarPlot($dataTotal);
-$bplot2->SetFillColor("red");
+
+$bplot2 = new BarPlot($dataFailedTotal);
+$bplot2->SetFillColor('blue');
 $bplot2->value->show();
 $bplot2->SetLegend("Failed");
-*/
-$gbplot = new GroupBarPlot(array($bplot1));
-$gbplot->SetWidth(0.4);
+
+$gbplot = new GroupBarPlot(array($bplot1, $bplot2));
+$gbplot->SetWidth(0.7);
 
 $graph->Add($gbplot);
 
