2e4f7824585e9a8c0915331b82200c1808d14cf9
adit <adit@addotech>
2015-01-09 17:12:51 +0700

    update function update password - getUpdatePassword()

--- classess/class.user.php
+++ classess/class.user.php
@@ -71,7 +71,7 @@ class USERS {
     
     public function getUpdatePassword() {
         $insert = false;
-        $sql_update_pass = "UPDATE USER_ADMIN SET PASSWORD_ADMIN '" . $this->getPassword() . "' where USERNAME_ADMIN = '" . $this->getUsername() . "' ";
+        $sql_update_pass = "UPDATE USER_ADMIN SET PASSWORD_ADMIN = '".$this->getPassword()."' WHERE USERNAME_ADMIN = '".$this->getUsername()."' ";
         $c=new ConnectionDB();
         $c->openConnection();
         $query_update_pass = mysql_query($sql_update_pass) or die (mysql_error());
