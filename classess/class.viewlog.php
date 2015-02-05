092ce23f8e8fb2b50bf07148d4c1c493f03eb1bb
adit <adit@adit-desktop>
2015-02-05 21:24:45 +0700

    new function getPaging()

--- classess/class.viewlog.php
+++ classess/class.viewlog.php
@@ -18,7 +18,7 @@ include dirname(__FILE__) . "/../config/config.db.php";
 
 class ViewLog {
 //put your code here
-    private $username, $ip, $status, $c ;
+    private $username, $ip, $status, $c, $offset, $limit, $page ;
     
     public function getUsername() {
         return $this->username;
@@ -26,6 +26,30 @@ class ViewLog {
 
     public function setUsername($username) {
         $this->username = $username;
+    }
+    
+     public function getOffset() {
+        return $this->offset;
+    }
+
+    public function setOffset($offset) {
+        $this->offset = $offset;
+    }
+    
+    public function getLimit() {
+        return $this->limit;
+    }
+
+    public function setLimit($limit) {
+        $this->limit = $limit;
+    }
+
+    public function getPage() {
+        return $this->page;
+    }
+
+    public function setPage($page) {
+        $this->page = $page;
     }
 
     // function view log
@@ -120,63 +144,13 @@ class ViewLog {
         return $query_failed;
     }
     
-    public function exportMysqlToCsv($filename = 'export.csv') {
-        $csv_terminated = "\n";
-        $csv_separator = ",";
-        $csv_enclosed = '"';
-        $csv_escaped = "\\";
-        $sql_query = "SELECT * FROM USER_LOG";
-
-        // Gets the data from the database
-        $result = mysql_query($sql_query);
-        $fields_cnt = mysql_num_fields($result);
-
-
-        $schema_insert = '';
-
-        for ($i = 0; $i < $fields_cnt; $i++) {
-            $l = $csv_enclosed . str_replace($csv_enclosed, $csv_escaped . $csv_enclosed, stripslashes(mysql_field_name($result, $i))) . $csv_enclosed;
-            $schema_insert .= $l;
-            $schema_insert .= $csv_separator;
-        } // end for
-
-        $out = trim(substr($schema_insert, 0, -1));
-        $out .= $csv_terminated;
-
-        // Format the data
-        while ($row = mysql_fetch_array($result)) {
-            $schema_insert = '';
-            for ($j = 0; $j < $fields_cnt; $j++) {
-                if ($row[$j] == '0' || $row[$j] != '') {
-
-                    if ($csv_enclosed == '') {
-                        $schema_insert .= $row[$j];
-                    } else {
-                        $schema_insert .= $csv_enclosed .
-                                str_replace($csv_enclosed, $csv_escaped . $csv_enclosed, $row[$j]) . $csv_enclosed;
-                    }
-                } else {
-                    $schema_insert .= '';
-                }
-
-                if ($j < $fields_cnt - 1) {
-                    $schema_insert .= $csv_separator;
-                }
-            } // end for
-
-            $out .= $schema_insert;
-            $out .= $csv_terminated;
-        } // end while
-
-        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-        header("Content-Length: " . strlen($out));
-        // Output to browser with appropriate mime type, you choose ;)
-        header("Content-type: text/x-csv");
-        //header("Content-type: text/csv");
-        //header("Content-type: application/csv");
-        header("Content-Disposition: attachment; filename=$filename");
-        echo $out;
-        exit;
+    public function getPaging() {
+        $sql_paging = "SELECT * FROM USER_LOG LIMIT ".$this->getOffset().",".$this->getLimit()." ";
+        $c = new ConnectionDB();
+        $c->openConnection();
+        $query_paging = mysql_query($sql_paging) or die (mysql_error());
+        return $query_paging;
+        
     }
 
 }
